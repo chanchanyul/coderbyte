a1d86d3e73332c7fa5401384c07da7066343360c
yoonjeen <yoonjeen@users.noreply.github.com>
2015-07-30 23:53:23 +0900

    Create TimeConvert.rb

---
+++ TimeConvert.rb
@@ -0,0 +1,29 @@
+def TimeConvert(num)
+
+  return "#{num/60.round}:#{num%60}"
+  
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+TimeConvert(STDIN.gets)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
